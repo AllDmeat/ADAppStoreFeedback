6c51cde259454f8fe9cb8d1fe1430e6a712499a9
Aleksey Berezka <alldmeat@icloud.com>
2019-10-17 04:15:39 +0500

    Removed redundant ‘public’ marks

--- Sources/AppStoreFeedback/AppStoreFeedDto+Convinience.swift
+++ Sources/AppStoreFeedback/AppStoreFeedDto+Convinience.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public extension AppStoreFeedDto {
-    public var entries: [FeedDto.EntryDto] { return feed.entry }
+    var entries: [FeedDto.EntryDto] { return feed.entry }
 }
 
 public extension AppStoreFeedDto.FeedDto.EntryDto {
-    public var id:      String { return idDto       .label }
-    public var title:   String { return titleDto    .label }
-    public var content: String { return contentDto  .label }
-    public var rating:  String { return ratingDto   .label }
+    var id:      String { return idDto       .label }
+    var title:   String { return titleDto    .label }
+    var content: String { return contentDto  .label }
+    var rating:  String { return ratingDto   .label }
 }
