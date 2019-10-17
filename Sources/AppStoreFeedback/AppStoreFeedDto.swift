354790ec04c62ef8d7c4d47aa68dabd1b9202957
Aleksey Berezka <alldmeat@icloud.com>
2019-10-18 01:07:05 +0500

    Beautified code

--- Sources/AppStoreFeedback/AppStoreFeedDto.swift
+++ Sources/AppStoreFeedback/AppStoreFeedDto.swift
@@ -7,22 +7,22 @@ public struct AppStoreFeedDto: Codable {
         let entry: [EntryDto]
         
         public struct EntryDto: Codable {
-            internal let idDto: EntryIdDto
-            internal let titleDto: EntryTitleDto
-            internal let contentDto: EntryContentDto
-            internal let ratingDto: EntryRatingDto
+            internal let idDto:         EntryIdDto
+            internal let titleDto:      EntryTitleDto
+            internal let contentDto:    EntryContentDto
+            internal let ratingDto:     EntryRatingDto
             
             enum CodingKeys: String, CodingKey {
-                case idDto = "id"
-                case titleDto = "title"
+                case idDto      = "id"
+                case titleDto   = "title"
                 case contentDto = "content"
-                case ratingDto = "im:rating"
+                case ratingDto  = "im:rating"
             }
         }
         
-        struct EntryIdDto: Codable { let label: String }
-        struct EntryTitleDto: Codable { let label: String }
+        struct EntryIdDto:      Codable { let label: String }
+        struct EntryTitleDto:   Codable { let label: String }
         struct EntryContentDto: Codable { let label: String }
-        struct EntryRatingDto: Codable { let label: String }
+        struct EntryRatingDto:  Codable { let label: String }
     }
 }
