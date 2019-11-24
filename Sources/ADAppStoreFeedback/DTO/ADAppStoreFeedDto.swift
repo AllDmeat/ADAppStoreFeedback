e44e0dced06c793d7ddbecfa1a45b5e13d67d44c
Aleksey Berezka <alldmeat@icloud.com>
2019-11-24 22:00:42 +0300

    Removed whitespaces

--- Sources/ADAppStoreFeedback/DTO/ADAppStoreFeedDto.swift
+++ Sources/ADAppStoreFeedback/DTO/ADAppStoreFeedDto.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 public class ADAppStoreFeedDto: Codable {
-    internal let entries: [ADEntryDto]
+    public let entries: [ADEntryDto]
     
     private enum CodingKeys: String, CodingKey {
         case feed
     }
     
     public required init(from decoder: Decoder) throws {
-        let container   = try decoder.container(keyedBy: CodingKeys.self)
+        let container = try decoder.container(keyedBy: CodingKeys.self)
         
         let feed = try container.decode(ADFeedDto.self, forKey: .feed)
         
